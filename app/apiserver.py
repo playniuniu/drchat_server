c1c59715714ff26a100c9af843ed653cace1f5b0
playniuniu <playniuniu@gmail.com>
2016-04-06 12:58:54 +0800

    finish api update

--- app/apiserver.py
+++ app/apiserver.py
@@ -4,7 +4,8 @@ import json
 from bottle import Bottle, response, request
 from lib.user import lib_user_login, lib_user_register
 from lib.contact import lib_add_contact, lib_get_contact, lib_delete_contact
-from lib.message import lib_get_message_history
+from lib.message import lib_get_message_history, lib_delete_message_history, \
+    lib_get_message_list, lib_delete_message_list
 
 app = Bottle()
 
@@ -82,3 +83,27 @@ def get_message_list(username):
 @app.route('/msghistory/<fromUser>/<toUser>', method='GET')
 def get_message_history(fromUser, toUser):
     return lib_get_message_history(fromUser, toUser)
+
+# 删除历史消息
+@app.route('/msghistory/<fromUser>/<toUser>', method=['OPTIONS', 'DELETE'])
+def delete_message_history(fromUser, toUser):
+    # 处理跨域访问中, 先发送 OPTIONS 的问题
+    if request.method == 'OPTIONS':
+        return {}
+
+    return lib_delete_message_history(fromUser, toUser)
+
+# 获取消息列表
+@app.route('/msglist/<fromUser>', method='GET')
+def get_message_list(fromUser):
+    return lib_get_message_list(fromUser)
+
+# 删除历史消息
+@app.route('/msglist/<fromUser>', method=['OPTIONS', 'DELETE'])
+def delete_message_list(fromUser):
+    # 处理跨域访问中, 先发送 OPTIONS 的问题
+    if request.method == 'OPTIONS':
+        return {}
+
+    toUser = request.params.get('username')
+    return lib_delete_message_history(fromUser, toUser)
--- lib/message.py
+++ lib/message.py
@@ -13,15 +13,6 @@ try:
 except ImportError:
     pass
 
-
-# 生成历史消息的 redis key
-def _message_history_redis_key(message):
-    parse_message = json.loads(message)
-    from_user = parse_message['fromUser']
-    to_user = parse_message['toUser']
-    redis_key = 'msghistory:' + min(from_user, to_user) + ":" + max(from_user, to_user)
-    return redis_key
-
 # 将消息推送到 redis queen
 def lib_send_redis_message(message, save_flag=True):
     try:
@@ -39,19 +30,47 @@ def lib_send_redis_message(message, save_flag=True):
 
 # 保存历史消息
 def lib_save_message_history(message):
-    redis_key = _message_history_redis_key(message)
+    parse_message = json.loads(message)
+    from_user = parse_message['fromUser']
+    to_user = parse_message['toUser']
+
+    # 保存两份历史消息, 一份自己用, 一份对方用
+    redis_key_local_history = 'msghistory:{}:{}'.format(from_user, to_user)
+    redis_key_remote_history = 'msghistory:{}:{}'.format(to_user, from_user)
+
+    # 保存两份消息列表
+    redis_key_local_msglist = 'msglist:{}'.format(from_user)
+    redis_key_remote_msglist = 'msglist:{}'.format(to_user)
 
     try:
         redis_client = redis.StrictRedis.from_url(config['REDIS_REMOTE_URL'])
-        redis_client.lpush(redis_key, message)
-        redis_client.ltrim(redis_key, 0, config['REDIS_HISTORY_LONG'])
+
+        # 使用 redis 的 mutli 操作
+        redis_pipeline = redis_client.pipeline()
+
+        # 保存本地消息
+        redis_pipeline.lpush(redis_key_local_history, message)
+        redis_pipeline.ltrim(redis_key_local_history, 0, config['REDIS_HISTORY_LONG'])
+
+        # 保存远端消息
+        redis_pipeline.lpush(redis_key_remote_history, message)
+        redis_pipeline.ltrim(redis_key_remote_history, 0, config['REDIS_HISTORY_LONG'])
+
+        # 保存本地消息列表
+        redis_pipeline.hset(redis_key_local_msglist, to_user, message)
+
+        # 保存远端消息列表
+        redis_pipeline.hset(redis_key_remote_msglist, from_user, message)
+
+        # 执行所有操作
+        redis_pipeline.execute()
     except:
         logging.error("ERROR! Cannot connect to {}".format(config['REDIS_REMOTE_URL']))
         return  False
 
 # 读取历史消息
 def lib_get_message_history(from_user, to_user):
-    redis_key = 'msghistory:' + min(from_user, to_user) + ":" + max(from_user, to_user)
+    redis_key = 'msghistory:{}:{}'.format(from_user, to_user)
     response = {}
 
     try:
@@ -65,4 +84,62 @@ def lib_get_message_history(from_user, to_user):
 
     response['status'] = 'ok'
     response['data'] = [ el.decode('utf-8') for el in redis_data ]
+    return response
+
+# 删除历史消息
+def lib_delete_message_history(from_user, to_user):
+    redis_key_message_history = 'msghistory:{}:{}'.format(from_user, to_user)
+    redis_key_message_list = 'msglist:{}'.format(from_user)
+    response = {}
+
+    try:
+        redis_client = redis.StrictRedis.from_url(config['REDIS_REMOTE_URL'])
+
+        # 删除历史消息
+        redis_client.delete(redis_key_message_history)
+
+        # 删除消息列表
+        redis_client.hdel(redis_key_message_list, to_user)
+    except:
+        logging.error("ERROR! Cannot connect to {}".format(config['REDIS_LOCAL_URL']))
+        response['status'] = 'err'
+        response['data'] = "连接数据库错误!"
+        return response
+
+    response['status'] = 'ok'
+    return response
+
+# 获取消息列表
+def lib_get_message_list(from_user):
+    redis_key = 'msglist:{}'.format(from_user)
+    response = {}
+
+    try:
+        redis_client = redis.StrictRedis.from_url(config['REDIS_LOCAL_URL'])
+        redis_data = redis_client.hvals(redis_key)
+    except:
+        logging.error("ERROR! Cannot connect to {}".format(config['REDIS_LOCAL_URL']))
+        response['status'] = 'err'
+        response['data'] = "连接数据库错误!"
+        return response
+
+    response['status'] = 'ok'
+    response['data'] = [json.loads(el.decode('utf-8')) for el in redis_data]
+    return response
+
+# 删除消息列表
+def lib_delete_message_list(from_user, to_user):
+    redis_key = 'msglist:{}'.format(from_user)
+    response = {}
+
+    try:
+        redis_client = redis.StrictRedis.from_url(config['REDIS_REMOTE_URL'])
+        redis_client.hdel(redis_key, to_user)
+    except:
+        logging.error("ERROR! Cannot connect to {}".format(config['REDIS_LOCAL_URL']))
+        response['status'] = 'err'
+        response['data'] = "连接数据库错误!"
+        return response
+
+    response['status'] = 'ok'
     return response
