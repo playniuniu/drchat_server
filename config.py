1543eda0cb730719500d558ca0a8d9b0e375fa3b
playniuniu <playniuniu@gmail.com>
2016-04-12 09:53:45 +0800

    add redis password

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ config = {
     'SOCKET_IO_CHANNEL': 'drchat',
     'SOCKET_IO_NAMESPACE' : '/drchat',
     # 设置 Redis
-    'REDIS_LOCAL_URL': 'redis://127.0.0.1:6379/0', # 本地 redis
-    'REDIS_REMOTE_URL': 'redis://drchat.uunus.cn:6379/0',  # 云端 redis
+    'REDIS_LOCAL_URL': 'redis://:dr123456@127.0.0.1:6379/0', # 本地 redis
+    'REDIS_REMOTE_URL': 'redis://:dr123456@drchat.uunus.cn:6379/0',  # 云端 redis
     'REDIS_HISTORY_LONG': 1000,
     # 设置 Debug
     'DEBUG_MODE' : 'True',
